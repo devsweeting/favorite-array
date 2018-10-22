9c9b078186b24bac33c16ed8cfbb01b93d4e934c
Devin Sweeting and Phil Mass <phil@>
2018-10-22 10:18:54 -0700

    create project & put input into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
+  $("#favorites").submit(function(event) {
+    var food = $("#food").val();
+    var movie = $("#movie").val();
+    var game = $("#game").val();
+    var team = $("#team").val();
+    var userInput = [food, movie, game, team];
 
+    var newArray = [userInput[1], userInput[0], userInput[2]];
+    console.log(userInput, newArray);
+    console.log(newArray);
+    event.preventDefault();
+  });
 });
